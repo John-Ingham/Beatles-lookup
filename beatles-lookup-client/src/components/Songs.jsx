2079d5d449b33b4f9156e49846bfce95485afba3
John-Ingham <johnaingham@yahoo.co.uk>
2022-01-18 17:24:13 +0000

    grabs data from BE api, made output for all songs display

--- beatles-lookup-client/src/components/Songs.jsx
+++ beatles-lookup-client/src/components/Songs.jsx
@@ -1,16 +1,58 @@
 import React from 'react'
 import sheetmusic2 from '../assets/sheetMusic2-asset.png'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { getData } from '../utils/api'
+import { Link } from 'react-router-dom'
 
 const Songs = () => {
+  const [loading, setLoading] = useState(true)
+  const [data, setData] = useState([])
+
   useEffect(() => {
-    getData()
+    setLoading(true)
+    getData().then((dataFromApi) => {
+      setData(dataFromApi)
+      setLoading(false)
+    })
   }, [])
   return (
     <div classname="songsbox">
       <img className="sheetMusicPic" src={sheetmusic2} alt="sheet music" />
-      <section>SONGS</section>
+      {loading ? (
+        <img
+          className="loading"
+          src="https://c.tenor.com/I6kN-6X7nhAAAAAj/loading-buffering.gif"
+          alt="Now Loading"
+        />
+      ) : (
+        <section>
+          <h2 className="header2s"> Songs List</h2>
+          <ul classname="lists">
+            {data.map((song) => {
+              return (
+                <li className="listItems" key={data.song}>
+                  <img
+                    className="musicThumbnail"
+                    src="https://cancerfocusni.org/wp-content/uploads/2019/10/Music-Notes-300-x-200-Event-page-thumbnail.png"
+                    alt="musical notes thumbnail"
+                    width="100"
+                    height="100"
+                  />
+                  <h3>SONG NAME: {song.song}</h3>
+                  <p>Written by: {song.writer}</p>
+                  <Link to={'lyrics'}>
+                    <button className="button">
+                      View Lyrics for {song.song}
+                    </button>
+                  </Link>
+
+                  <span>{song.lyrics}</span>
+                </li>
+              )
+            })}
+          </ul>
+        </section>
+      )}
     </div>
   )
 }
--- beatles-lookup-client/src/utils/api.js
+++ beatles-lookup-client/src/utils/api.js
@@ -1,26 +1,22 @@
 // All api requests
 import { useEffect } from 'react'
 import axios from 'axios'
+const api = axios.create({
+  baseURL: 'https://beatles-music-api.herokuapp.com/',
+})
 
 export const getData = async () => {
   console.log('>>>In function')
-  fetch('https://beatles-api.herokuapp.com/api')
-    // await axios
-    //   .get(
-    //     'https://beatles-api.herokuapp.com/api'),
-    //, {
-    //   mode: 'cors',
-    //   headers: { 'Access-Control-Allow-Origin': '*' },
-    // }
-
-    //   try {
-    //     const response = await fetch('https://beatles-api.herokuapp.com/', {
-    //       mode: 'cors',
-    //       headers: { 'Access-Control-Allow-Origin': '*' },
-    //     })
-    // const data = await response.json()
-
-    .then((response) => {
-      console.log(response.data)
-    })
+  const { data } = await api.get('/')
+  console.log(data.data[0])
+  console.log(data.data[0].song)
+  return data.data
 }
+
+// fetch('https://beatles-api.herokuapp.com/', {
+//   method: 'GET',
+//   headers: { 'Access-Control-Allow-Origin': '*' },
+//   mode: 'cors',
+// }).then((response) => {
+//   console.log(response.data)
+// })}
